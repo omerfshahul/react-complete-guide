91a2f1598a8f63d8073c3cc350902a26b020e212
omerfshahul <omer.f.shahul@gmail.com>
2018-03-05 08:44:48 -0500

    changes to styles

--- src/App.js
+++ src/App.js
@@ -67,7 +67,8 @@ class App extends Component {
   }
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font: 'inherit',
       border: '2x solid blue',
       padding: '8px',
@@ -88,11 +89,21 @@ class App extends Component {
           })}
        </div>
       );
+      style.backgroundColor = 'red';
     }
+    let classes = [];
+
+    if( this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+  
     return (
       <div className="App">
         <h1>Hi! I am the react app! </h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.togglePersons}>Switch Name</button>
         {persons}
        
