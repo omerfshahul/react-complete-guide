3277d97aea2a2b8d0afaadef97d9471f59e490d7
omerfshahul <omer.f.shahul@gmail.com>
2018-03-07 15:00:05 -0500

    error throw

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,12 @@ import styles from './Person.css';
 
 const person = (props) => {
   
-  
+    const rnd = Math.random();
+    if (rnd > 0.7){
+        throw new Error('Something went wrong')
+    }
     return (
+        
         <div className={styles.Person} >
 
             <p onClick={props.click}>I'm a {props.name}! and I am {props.age} years old!</p>
