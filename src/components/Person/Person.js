a670f9df447c1a5f62250e6af06f8fcc3b49e7e5
omerfshahul <omer.f.shahul@gmail.com>
2018-03-21 19:58:21 -0400

    old mac changes

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -6,7 +6,7 @@ class Person extends Component{
         return( 
         <div className={styles.Person} >
 
-        <p onClick={this.props.click}>I'm a {this.props.name}! and I am {props.age} years old!</p>
+        <p onClick={this.props.click}>I'm a {this.props.name}! and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value = {this.props.name} />
     </div>
